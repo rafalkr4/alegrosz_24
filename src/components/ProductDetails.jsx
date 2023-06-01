c8fc34bb65fa611373757a796e98b011884db61f
Rafał Krasowski <rrafal.ka@gmail.com>
2023-06-01 15:34:57 +0200

    cart using context API

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import {
     Button,
@@ -12,11 +12,13 @@ import {
 import CardActions from "@mui/material/CardActions";
 import { faker } from "@faker-js/faker";
 import Container from "@mui/material/Container";
+import { CartContext } from "../context/CartContext.jsx";
 
 function ProductDetails() {
     const [product, setProduct] = useState({});
     const { productId } = useParams();
     const navigate = useNavigate();
+    const { setCart } = useContext(CartContext);
 
     useEffect(() => {
         getProduct(productId).then((product) => setProduct(product));
@@ -36,6 +38,26 @@ function ProductDetails() {
     async function deleteProduct() {
         await deleteProductAPI(productId);
         navigate("/");
+    }
+
+    async function updateStock(id, quantity) {
+        const response = await fetch(`http://localhost:3000/products/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                quantity,
+            }),
+        });
+
+        return response.json();
+    }
+
+    async function buyProduct() {
+        setCart((prev) => prev + product.price);
+        setProduct({ ...product, quantity: product.quantity - 1 });
+        await updateStock(productId, product.quantity - 1);
     }
 
     return (
@@ -100,6 +122,13 @@ function ProductDetails() {
                                     variant="outlined"
                                 >
                                     Delete
+                                </Button>
+                                <Button
+                                    onClick={buyProduct}
+                                    size="small"
+                                    variant="outlined"
+                                >
+                                    Buy
                                 </Button>
                             </CardActions>
                         </Card>
