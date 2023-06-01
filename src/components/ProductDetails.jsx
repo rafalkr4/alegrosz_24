d92472662e5ae13c98e70673aa0b9824762aafd0
Rafał Krasowski <rrafal.ka@gmail.com>
2023-06-01 13:04:18 +0200

    class component for updating product

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -79,20 +79,26 @@ function ProductDetails() {
                             </CardContent>
                             <CardActions>
                                 <Link to={"/"}>
-                                    <Button size="small">Back</Button>
+                                    <Button size="small" variant="outlined">
+                                        Back
+                                    </Button>
                                 </Link>
 
                                 <Link to={`/product-edit/${product.id}`}>
                                     <Button
                                         size="small"
-                                        variant="outline"
+                                        variant="outlined"
                                         sx={{ marginRight: 1 }}
                                     >
                                         Edit {product.name}
                                     </Button>
                                 </Link>
 
-                                <Button onClick={deleteProduct} size="small">
+                                <Button
+                                    onClick={deleteProduct}
+                                    size="small"
+                                    variant="outlined"
+                                >
                                     Delete
                                 </Button>
                             </CardActions>
